3eecfc9b89ad8c1391d9567d6498546d76948c2e
Aleksandr Vyalov <a.vyalov@vconnect.ru>
2018-12-26 16:25:21 +0300

    add file

---
+++ application/models/M_shapingPage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a.vyalov
+ * Date: 26.12.2018
+ * Time: 14:53
+ */
+
+class M_shapingPage extends CI_Model {
+
+    public function addBodyElementFromView(){
+        echo 322;
+    }
+
+}
