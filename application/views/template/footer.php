e337fbf981c640207ae2e4394c038f71dce2be8f
Aleksandr Vyalov <a.vyalov@vconnect.ru>
2018-12-26 10:28:17 +0300

    Initial commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a.vyalov
+ * Date: 25.12.2018
+ * Time: 14:55
+ */
+?>
+
+<div class="footer">
+    <p>footer</p>
+</div>
---
+++ application/views/template/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a.vyalov
+ * Date: 25.12.2018
+ * Time: 14:54
+ */
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../../bootstrap/css/bootstrap.css">
+    <script src="../../../bootstrap/js/jquery.min.js"></script>
+    <script src="../../../bootstrap/js/bootstrap.js"></script>
+</head>
+
+<div class="container-fluid">
+    <p>header</p>
+</div>
+
